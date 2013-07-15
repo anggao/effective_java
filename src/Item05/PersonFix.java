2143e4e7ba5da01c3a1ed98bdfbf27dc3c7d36ee
Ang Gao <ang.gao87@gmail.com>
2013-07-15 01:03:37 +0100

    Item 05: Avoid creating unnecessary objects

---
+++ src/Item05/PersonFix.java
@@ -0,0 +1,33 @@
+package Item05;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class PersonFix {
+	private final Date birthDate;
+	// Other fields, methods, and constructor omitted
+
+	public PersonFix(Date birthDate){
+		this.birthDate = birthDate;
+	}
+	/**
+	 * The starting and ending dates of the baby boom.
+	 */
+	private static final Date BOOM_START;
+	private static final Date BOOM_END;
+
+	static {
+		Calendar gmtCal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+		gmtCal.set(1946, Calendar.JANUARY, 1, 0, 0, 0);
+		BOOM_START = gmtCal.getTime();
+		gmtCal.set(1965, Calendar.JANUARY, 1, 0, 0, 0);
+		BOOM_END = gmtCal.getTime();
+	}
+
+	public boolean isBabyBoomer() {
+		return birthDate.compareTo(BOOM_START) >= 0
+				&& birthDate.compareTo(BOOM_END) < 0;
+	}
+
+}
